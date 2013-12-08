a8b691df8d17c996b43b03eb42aa55d622273587
Trismegiste <flo@trismegiste.net>
2013-12-08 18:20:07 +0100

    add tests for decorator

---
+++ tests/Yuurei/Persistence/DecoratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Yuurei
+ */
+
+namespace tests\Yuurei\Persistence;
+
+/**
+ * DecoratorTest tests the decorator
+ */
+class DecoratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $sut;
+
+    protected function setUp()
+    {
+        $mock = $this->getMock('Trismegiste\Yuurei\Persistence\RepositoryInterface');
+        $this->sut = $this->getMockForAbstractClass('Trismegiste\Yuurei\Persistence\Decorator', [$mock]);
+    }
+
+    public function testQuery()
+    {
+        $this->sut->find();
+        $this->sut->findOne();
+        $this->sut->findByPk(123);
+        $this->sut->getCursor();
+    }
+
+    public function testPersistence()
+    {
+        $this->sut->createFromDb([]);
+        $this->sut->persist($this->getMock('Trismegiste\Yuurei\Persistence\Persistable'));
+    }
+
+}
